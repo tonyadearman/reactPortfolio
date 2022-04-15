13721b719b7948a8668210fedd0e6b7f463c2e7c
tonyadearman <dearman.tonya@gmail.com>
2022-04-15 06:57:18 -0700

    Edited Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import About from "./components/about/About"
 import Intro from "./components/intro/Intro"
+import Contact from "./components/contact/Contact"
 
 const App = ()  => {
   return (
     <div className="App">
       <Intro />
       <About />
+      <Contact />
+    
     </div>
   );
 }
